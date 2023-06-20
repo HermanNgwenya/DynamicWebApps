6253fe46f2cc040d52dd67bd060f7aedd197932a
HermanNgwenya <hermanbngwenya@gmail.com>
2023-06-20 22:28:24 +0200

    dwa_09

--- DWA_09/scripts.js
+++ DWA_09/scripts.js
@@ -40,28 +40,15 @@ const selectors = {
  * @returns {element} 
  */ 
 
-// class BookPreview {
-//   constructor(book) {
-//     this.book = book;
-//     this.element = this.createPreviewElement();
-//   }
-
-//  Custeom BookPreview component
 class BookPreview extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: 'open' });
     this.book = null;
-    this.render();
   }
-
 
   connectedCallback() {
-    this.addEventListener('click', this.handlePreviewClick);
-  }
-
-  disconnectedCallback() {
-    this.removeEventListener('click', this.handlePreviewClick);
+    this.render();
   }
 
   static get observedAttributes() {
@@ -75,84 +62,93 @@ class BookPreview extends HTMLElement {
     }
   }
 
-  handlePreviewClick() {
-    // Custom event to dispatch when the preview is clicked
-    const event = new CustomEvent('previewClick', {
-      bubbles: true,
-      detail: {
-        book: this.book,
-      },
-    });
-    this.dispatchEvent(event);
+  createPreviewElement() {
+    const { author, id, image, title } = this.book;
+    const element = document.createElement('button');
+    element.classList = 'preview';
+    element.setAttribute('data-preview', id);
+
+    element.innerHTML = `
+      <img class="preview__image" src="${image}" />
+      <div class="preview__info">
+        <h3 class="preview__title">${title}</h3>
+        <div class="preview__author">${authors[author]}</div>
+      </div>
+    `;
+
+    return element;
   }
 
   updatePreviewContent() {
-    if (this.book) {
-      const { author, image, title } = this.book;
-      this.shadowRoot.querySelector('.preview__image').src = image;
-      this.shadowRoot.querySelector('.preview__title').innerText = title;
-      this.shadowRoot.querySelector('.preview__author').innerText = authors[author];
-    }
+    const { author, image, title } = this.book;
+    this.shadowRoot.querySelector('.preview__image').src = image;
+    this.shadowRoot.querySelector('.preview__title').innerText = title;
+    this.shadowRoot.querySelector('.preview__author').innerText = authors[author];
   }
 
   render() {
-    const template = document.createElement('template');
-    template.innerHTML = `
+    this.shadowRoot.innerHTML = `
       <style>
-        /* Styles for the book preview component */
+      .preview {
+        border-width: 0;
+        width: 100%;
+        font-family: Roboto, sans-serif;
+        padding: 0.5rem 1rem;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        text-align: left;
+        border-radius: 8px;
+        border: 1px solid rgba(var(--color-dark), 0.15);
+        background: rgba(var(--color-light), 1);
+      }
+      @media (min-width: 60rem) {
+        .preview {
+          padding: 1rem;
+        }
+      }
+      
+      .preview_hidden {
+        display: none;
+      }
+      
+      .preview:hover {
+        background: rgba(var(--color-blue), 0.05);
+      }
+      
+      .preview__image {
+        width: 48px;
+        height: 70px;
+        object-fit: cover;
+        background: grey;
+        border-radius: 2px;
+        box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+          0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+      }
+      
+      .preview__info {
+        padding: 1rem;
+      }
+      .preview__title {
+        margin: 0 0 0.5rem;
+        font-weight: bold;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;  
+        overflow: hidden;
+        color: rgba(var(--color-dark), 0.8)
+      }
+      
+      .preview__author {
+        color: rgba(var(--color-dark), 0.4);
+      }
       </style>
-      <button class="preview">
-        <img class="preview__image" />
-        <div class="preview__info">
-          <h3 class="preview__title"></h3>
-          <div class="preview__author"></div>
-        </div>
-      </button>
+      ${this.createPreviewElement().outerHTML}
     `;
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 };
 
-// Register the custom element
-window.customElements.define('book-preview', BookPreview);
-
-//   createPreviewElement() {
-//     const { author, id, image, title } = this.book;
-//     const element = document.createElement('button');
-//     element.classList = 'preview';
-//     element.setAttribute('data-preview', id);
-
-//     element.innerHTML = `
-//       <img class="preview__image" src="${image}" />
-//       <div class="preview__info">
-//         <h3 class="preview__title">${title}</h3>
-//         <div class="preview__author">${authors[author]}</div>
-//       </div>
-//     `;
-
-//     return element;
-//   }
-
-//   getElement() {
-//     return this.element;
-//   }
-
-//   getBook() {
-//     return this.book;
-//   }
-
-//   updateBook(book) {
-//     this.book = book;
-//     this.updatePreviewContent();
-//   }
-
-//   updatePreviewContent() {
-//     const { author, image, title } = this.book;
-//     this.element.querySelector('.preview__image').src = image;
-//     this.element.querySelector('.preview__title').innerText = title;
-//     this.element.querySelector('.preview__author').innerText = authors[author];
-//   }
-// }
+customElements.define('book-preview', BookPreview);
 
 // Modify the createBookPreview function to use the BookPreview class
 function createBookPreview(book) {
@@ -177,7 +173,7 @@ function handleListButtonClicked() {
   const end = (page + 1) * BOOKS_PER_PAGE;
 
   const previews = matches.slice(start, end).map((book) => {
-    const preview = createBookPreview(book);
+    const previewElement = createBookPreview(book);
     fragment.appendChild(previewElement);
     return previewElement;
   });
