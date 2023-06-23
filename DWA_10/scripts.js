91aaf40e06742079e7257c53975e787d2b8ab373
HermanNgwenya <hermanbngwenya@gmail.com>
2023-06-23 13:07:41 +0200

    DWA 10

--- DWA_10/scripts.js
+++ DWA_10/scripts.js
@@ -1,3 +1,55 @@
+const MAX_NUMBER = 15
+const MIN_NUMBER = -5
+
 const number = document.querySelector ('[data-key="number"]')
 const subtract =  document.querySelector ('[data-key="subtract"]')
 const add =  document.querySelector ('[data-key="add"]')
+const refresh = document.querySelector ('[data-key="refresh"]')
+
+const subtractHandler = () => {
+    const newValue = parseInt(number.value) - 1
+    number.value = newValue
+
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true
+    }
+}
+
+const addHandler = () => {
+    const newValue = parseInt(number.value) + 1
+    number.value = newValue
+
+    if (subtract.disabled === true) {
+        subtract.disabled = false
+    }
+
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
+
+const refreshHandler = () => {
+    number.value = 0;
+    add.disabled = false;
+    subtract.disabled = false;
+    showMessage("The counter has been reset");
+}
+
+const showMessage = (message) => {
+    alert(message);
+  };
+
+subtractHandler()
+addHandler()
+refreshHandler()
+
+
+subtract.addEventListener('click', subtractHandler)
+
+add.addEventListener('click', addHandler )
+
+refresh.addEventListener('click',refreshHandler)
